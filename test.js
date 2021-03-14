0b4440a7f077b608d00c42ec35ffb0c04c0b8a82
Fabian Barthold <79047433+FabFrusciante@users.noreply.github.com>
2021-03-14 11:50:21 +0100

    Add test.js for unit testing

---
+++ test.js
@@ -0,0 +1,75 @@
+// Input stringSequence dynamically
+// Input text dynamically
+// Konverting text to array
+// Outsourcing of helper functions
+// Formatting with style guide
+
+function longestWord(word_list) {
+    let longestWord = '';
+    for (var element of word_list) {
+        if (element.length > longestWord.length) {
+            longestWord = element;
+        }
+    }
+    return longestWord;
+};
+
+function mapWord(word) {
+    let map = {};
+    for (let i = 0; i < word.length; i++) {
+        let letter = word[i];
+        if (map[letter]) {
+            map[letter].push(i);
+        } else {
+            map[letter] = [i];
+        }
+    }
+    return map;
+};
+
+function findNextIndex(array, minIndex) {
+    for (var element of array) {
+        if (element >= minIndex) {
+            return element + 1;
+        }
+    }
+    return false;
+};
+
+function isSubsequence(word, map) {
+    let minIndex = 0;
+    for (let letter of word) {
+        if (map[letter]) {
+            minIndex = findNextIndex(map[letter], minIndex);
+            if (minIndex == false) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
+};
+
+function longestMatch(word, word_list) {
+    let mappedWord = mapWord(word);
+    let subsequences = [];
+    for (var element of word_list) {
+        if (isSubsequence(element, mappedWord)) {
+            subsequences.push(element);
+        }
+    }
+    let longestSubsequence = longestWord(subsequences);
+    return longestSubsequence;
+};
+
+let stringSequence = 'abppplee';
+let dictionary = [
+    'able',
+    'ale',
+    'apple',
+    'bale',
+    'kangaroo'
+];
+
+console.log(longestMatch(stringSequence, dictionary));
